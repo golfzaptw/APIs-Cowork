b9f845ce6851987c679ed258138c7739a80d4a14
patawee <psgolf16@gmail.com>
2018-03-20 18:31:52 +0700

    Test CI

---
+++ Test Suites/TestCI.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestCI</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>10b8675f-ae6d-439c-a793-10c7b785c8ce</testSuiteGuid>
+   <testCaseLink>
+      <guid>83740bd5-7cc4-4213-8dfd-328f47e27288</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/List/list_cowork</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>556ec059-9c36-4a33-817b-b055503d968b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/admin_login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0e7bf84e-8ced-4fe7-8795-57ff71e72413</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/email_login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c55f97df-bfa6-477f-8103-f75d0e1cfc7d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/facebook_login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>65dfe558-aff9-4e5d-9161-b4b24c8bfde4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/register_provider</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3b8f0049-1afb-4969-a4d1-d5a48c7accb5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/register_user</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
