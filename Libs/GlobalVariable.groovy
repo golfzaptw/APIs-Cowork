d84e1738168714937af30c8ebba0ce9643af20dc
patawee <psgolf16@gmail.com>
2018-03-21 16:04:36 +0700

    Add Global Variable for check response key

--- Libs/GlobalVariable.groovy
+++ Libs/GlobalVariable.groovy
@@ -21,4 +21,24 @@ public class GlobalVariable {
      */
 	public static Object server = 'https://api-co-work.herokuapp.com/'
 	 
+    /**
+     * <p></p>
+     */
+	public static Object list_response = ['success', 'data', '_id', 'name', 'rarting', 'address', 'gellery', 'status']
+	 
+    /**
+     * <p></p>
+     */
+	public static Object emailLogin_response = ['success', 'data', 'id', 'name', 'email', 'status', 'image']
+	 
+    /**
+     * <p></p>
+     */
+	public static Object fbLogin_response = ['success', 'data', '_id', 'name', 'email', 'password', 'image', 'status', 'facebook_id']
+	 
+    /**
+     * <p></p>
+     */
+	public static Object adminLogin_response = ['success', 'data', '_id', 'email', 'password', 'role']
+	 
 }
