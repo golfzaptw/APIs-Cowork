419046810e90d05fcfff0334057be567565796cc
patawee <psgolf16@gmail.com>
2018-03-21 16:04:06 +0700

    TestSuite

--- Test Suites/APIsTestSuite/APIs Login.ts
+++ Test Suites/APIsTestSuite/APIs Login.ts
@@ -4,7 +4,7 @@
    <name>APIs Login</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-03-20T17:02:45</lastRun>
+   <lastRun>2018-03-21T15:58:53</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
